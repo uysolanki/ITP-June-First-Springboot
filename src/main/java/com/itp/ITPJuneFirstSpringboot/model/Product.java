84ddfc59ce1fd74a5f661f8e1d1785dd9c03004d
uysolanki <uysolanki@gmail.com>
2025-10-28 12:32:44 +0530

    created at

--- src/main/java/com/itp/ITPJuneFirstSpringboot/model/Product.java
+++ src/main/java/com/itp/ITPJuneFirstSpringboot/model/Product.java
@@ -1,11 +1,16 @@
 package com.itp.ITPJuneFirstSpringboot.model;
 
-import java.util.List;
+import java.time.LocalDateTime;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +22,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Product 
 {
 	@Id
@@ -26,5 +32,25 @@ public class Product
 	private String productDesc;
 	private String productCategory;
 	private double price;
+	
+	private LocalDateTime createdAt;
+	
+    private LocalDateTime modifiedAt;
+	
+	@PrePersist
+	protected void atCreation()
+	{
+		LocalDateTime now=LocalDateTime.now();
+		this.createdAt=now;
+		this.modifiedAt=now;
+	}
+	
+	@PreUpdate
+	protected void atUpdation()
+	{
+		this.modifiedAt=LocalDateTime.now();
+	}
+
+
 }
 
